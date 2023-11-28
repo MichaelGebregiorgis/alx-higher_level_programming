3cfa12e62746c8f1f070d33cd2006b6ffec1928d
Michael Gebregiorgis <you@example.com>
2023-11-28 00:49:43 -0800

    digit from 00 to 99

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 for num in range(0,100):
-    print(f"{int(num / 10)}{int(num % 10)}")
+    if num == 99:
+        break
+    print(f"{int(num / 10)}{int(num % 10)}", end=', ')
+print("99")
