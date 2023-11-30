8c4a3d025499c48c10571a46465e9d3109a6e711
Michael Gebregiorgis <you@example.com>
2023-11-30 15:46:03 -0800

    Adder

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    length = len(sys.argv)
-    for args in range(length):
-        nArg = args + 1
-        if args > 0:
-            print(f"{int(sys.argv[args]) + int(sys.argv[nArg])}")
+    arguments = sys.argv[1:]
+    sumVal = sum(int(args) for args in arguments)
+    print(sumVal)
