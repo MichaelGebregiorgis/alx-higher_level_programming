a8af9712f236b89ccd2f286a2444607e3157614a
Michael Gebregiorgis <you@example.com>
2023-11-30 16:17:37 -0800

    alphabet by importing

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,23 @@
+import sys
+from calculator_1 import add, sub, mul, div
+if __name__ == "__main__":
+    length = len(sys.argv)
+    if length == 4 or sys.argv[2] == '*':
+        op = sys.argv[2]
+        val1 = int(sys.argv[1])
+        val2 = int(sys.argv[3])
+        if op == '+' or op == '-' or op == '*' or op == '/':
+            if op == '+':
+                print(f"{val1} + {val2} = {add(val1, val2)}")
+            elif op == '-':
+                print(f"{val1} - {val2} = {sub(val1, val2)}")
+            elif op == '*':
+                print(f"{val1} * {val2} = {mul(val1, val2)}")
+            elif op == '/':
+                print(f"{val1} / {val2} = {div(val1, val2)}")
+        else:
+            print("Unknown operator. Available operators: +, -, * and /")
+            sys.exit(1)
+    elif length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
