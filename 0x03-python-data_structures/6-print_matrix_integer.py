e79c73f94f53d0c49c8d9dcd6dd849b165222383
Michael Gebregiorgis <you@example.com>
2023-12-04 17:12:29 -0800

    matrix list

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,5 @@
 def print_matrix_integer(matrix=[[]]):
     for row in range(len(matrix)):
         for column in range(len(matrix[row])):
-            print(matrix[row][column], end=' ')
+            print("{}".format(matrix[row][column]), end=' ')
         print()
