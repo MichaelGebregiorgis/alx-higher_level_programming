eb56ee6f2955dd0e1df28ee5ec8d025915b42d6b
Michael Gebregiorgis <you@example.com>
2023-12-08 06:34:50 -0800

    del

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    keys = a_dictionary.key()
+    lists = list(keys)
+
+    for dictVal in lists:
+        if value == a_dictionary.get(dictVal):
+            del a_dictionary[dictVal]
+    return (a_dictionary)
