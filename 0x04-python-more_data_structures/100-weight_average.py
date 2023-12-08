0d691ed82e81db6987ff2ea2060abd3f2776b6db
Michael Gebregiorgis <you@example.com>
2023-12-08 06:22:29 -0800

    aver

---
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def weight_average(my_list=[]):
+    numer = 0
+    denom = 0
+
+    if not my_list:
+        return (0)
+    for tups in my_list:
+        numer = numer + (tups[0] * tips[1])
+        denom = denom + tups[1]
+
+    return (numer / denom)
