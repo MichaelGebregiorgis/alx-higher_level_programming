6f24a57c313a393c8807f687882f3f545a4312aa
Michael Gebregiorgis <you@example.com>
2023-12-18 15:28:54 -0800

    error print

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    try:
+        print("{:d}".format(value))
+        return (True)
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exec_info()[1]), file = sys.stderr)
+        return (False)
