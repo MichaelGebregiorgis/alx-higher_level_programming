60912d1273d18b4f92c73a66305be419c5003385
Michael Gebregiorgis <you@example.com>
2023-12-19 11:06:02 -0800

    with area

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""square with safety"""
+"""area is added now"""
 
 
 class Square:
-    """in the class"""
+    """inside the class"""
     def __init__(self, val=0):
-        """size with exceptions"""
+        """exceprtion"""
         if not isinstance(val, int):
             raise TypeError("size must be an integer")
         elif val < 0:
             raise ValueError("size must be >= 0")
         self.__size = val
+    def area(self):
+        """area"""
+        return (self.__size * self.__size)
