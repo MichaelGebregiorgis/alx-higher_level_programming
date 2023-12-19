60df351416aae1d1259b3c00529c007fa5c88d21
Michael Gebregiorgis <you@example.com>
2023-12-19 10:07:06 -0800

    square with size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""private instance attribute with square"""
+
+
+class Square:
+    """inside the square class"""
+    def __init__(self, val):
+        """initialization"""
+        self.__size = val
