820aa8578b781089c4867af5fe324e39abdb619e
Michael Gebregiorgis <you@example.com>
2024-01-04 12:36:55 -0800

    Rectangle shape

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+"""Rect class"""
+
+
+class Rectangle:
+    """Rectangle detail"""
+    def __init__(self, wid = 0, hgt = 0):
+        """initialization"""
+        self. hgt = hgt
+        self.wid = wid
+
+    @property
+    def wid(self):
+        """width of the rectangle"""
+        return (self._wid)
+
+    @width.setter
+    def wid(self, val):
+        if val < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(val, int):
+            raise TypeError("width must be an integer")
+        self.__wid = val
+
+    @property
+    def hgt(self):
+        """Height of the rectangle"""
+        return (self.hgt)
+    @hgt.setter
+    def hgt(self, val):
+        if val < 0:
+            raise ValueError("height must be >= 0")
+        if not isinstance(val, int):
+            raise TypeError("height must be an integer")
+        self.__hgt = val
