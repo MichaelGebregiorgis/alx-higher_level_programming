4f2a2e6d6e398a0317a12817d957bbde9fe98e22
Michael Gebregiorgis <you@example.com>
2024-01-06 17:56:58 -0800

    area + per

---
+++ 0x08-python-more_classes/2-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define class"""
+
+
+class Rectangle:
+    """Describe rect"""
+
+    def __init__(self, width=0, height=0):
+        """Initialization"""
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """width"""
+        return (self.__width)
+
+    @width.setter
+    def width(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.__width = value
+
+    @property
+    def height(self):
+        """height"""
+        return (self.__height)
+
+    @height.setter
+    def height(self, value):
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        self.__height = value
+
+    def perimeter(self):
+        """return perimeter"""
+        if self.width == 0 or self.__height == 0:
+            return(0)
+        return(2 * (self.__width + self.__height))
+
+    def area(self):
+        """Return area"""
+        return (self.__width * self.__height)
