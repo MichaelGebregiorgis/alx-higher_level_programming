a64c793050315c028f6d920930331fe4a2507366
Michael Gebregiorgis <you@example.com>
2024-01-09 14:20:47 -0800

    load add and save

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Add to python list"""
+import sys
+
+if __name__ == "__main__":
+    load_file = __import__('6-load_from_json_file').load_from_json_file
+    save_file = __import__('5-save_to_json_file').save_to_json_file
+
+    try:
+        val = load_file("add_item.json")
+    except FileNotFounfError:
+        val = []
+    val.extend(sys.argv[1:])
+    save_file(val, "add_item.json")
