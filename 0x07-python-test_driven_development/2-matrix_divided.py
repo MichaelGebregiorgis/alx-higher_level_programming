dfc4ef3935ddddb3a07cdc0a03b0905cd3cc1eb8
Michael Gebregiorgis <you@example.com>
2024-01-09 12:48:00 -0800

    It works

---
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Division function"""
+
+
+def matrix_divided(matrix, div):
+    """Divide values inside the matrix"""
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    if (matrix == [] or not isinstance(matrix, list) or
+            not all(isinstance(row, list) for row in matrix) or
+            not all((isinstance(ele, float) or isinstance(ele, int)) for ele in [num for row in matrix for num in row])):
+                raise TypeError("matrix must be a matrix (list of lists) of ""integers/floats")
+
+    if div == 0:
+        raise ZeroDivisionError("division by zero")
+    if not isinstance(div, int) and not isinstance(div, float):
+        raise TypeError("div must be a number")
+    return ([list(map(lambda x: round(x / div, 2), row)) for row in matrix])
