7451404763acb5f1a3d5fe90263a853b0850ff20
Michael Gebregiorgis <you@example.com>
2024-01-09 23:15:14 -0800

    triangle

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,18 @@
+#!usr/bin/python3
+"""Pascal's triangle"""
+
+
+def pascal_triangle(n):
+    """Representation"""
+    if n <= 0:
+        return ([])
+
+    tri = [[1]]
+    while len(tri) != n:
+        prev = tri[-1]
+        curr = [1]
+        for inc in range(len(prev) - 1):
+            curr.append(prev[inc] + prev[inc + 1])
+        curr.append(1)
+        tri.append(curr)
+    return (tri)
