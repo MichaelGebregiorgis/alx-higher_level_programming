6c3a5d90de4cebb70c5844512b678bf17dea5937
Michael Gebregiorgis <you@example.com>
2024-01-09 21:41:33 -0800

    student

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Student class"""
+
+
+class Student:
+    """Class representation"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialization"""
+        self.age = age
+        self.firsst_name = first_name
+        self.last_name = last_name
+
+    def to_json(self, attrs=None):
+        """Dctionary of student class"""
+        if(all(type(ele) == str for ele in attrs) and type(attrs) == list):
+            return ({x: getattr(self, x) for x in attrs if hasattr(self, k)})
+        return (self.__dict__)
