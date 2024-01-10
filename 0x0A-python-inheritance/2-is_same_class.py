eab3935a09668c3ae3e6f33ccb633442d7d09c2c
Michael Gebregiorgis <you@example.com>
2024-01-10 11:32:23 -0800

    same class?

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class checker function"""
+
+
+def is_same_class(obj, a_class):
+    """instance check"""
+    if type(obj) == a_class:
+        return (True)
+    return (False)
