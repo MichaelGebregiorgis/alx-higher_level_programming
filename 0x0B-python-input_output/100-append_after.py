bfb459842046d708ea4dbf7e48bff2fda29cb77f
Michael Gebregiorgis <you@example.com>
2024-01-09 23:29:55 -0800

    Appendetr

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
+"""inserts a line of text to a file"""
 
+
+def append_after(filename="", search_string="", new_string=""):
+    """Insert after each line of string"""
+    txt = ""
+    with open(filename) as rd:
+        for line in rd:
+            txt += line
+            if search_string in line:
+                txt += new_sring
+    with open(filename, 'w') as wte:
+        wte.write(txt)
