2f3f866a6a713f6b3d2f82582e8523b7af9409ac
Michael Gebregiorgis <you@example.com>
2024-01-17 12:01:27 -0800

    base file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -12,3 +12,41 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """Json of list"""
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to _file(cls, list_objs):
+        """Save json"""
+        name = cls.__name__ + ".json"
+        with open(name, "w") as jfile:
+            if list_objs is None:
+                jfile.write("[]")
+            else:
+                list_dicts = [opn.todictionary() for opn in list_objs]
+
+    @staticmethod
+    def from_json_string(json_string):
+        """json string list"""
+        if json_string is None or json_string == "[]":
+            return[]
+        return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        """return instance"""
+        from models.rectangle import Rectangle
+        from models.square import Square
+
+        if cls.__name__ == 'Rectangle':
+            dmy = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dmy = cls(1)
+
+        dmy.update(**dictionary)
+        return (dmy)
