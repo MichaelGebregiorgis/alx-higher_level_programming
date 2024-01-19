627a745f72640ec61585db04168f52e45818c5c8
Michael Gebregiorgis <you@example.com>
2024-01-19 12:19:15 -0800

    Representation

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -5,10 +5,7 @@
 class Square(Rectangle):
     """Repersent values"""
     def __init__(self, size, x=0, y=0, id=None):
-        self.size = size
-        self.x = x
-        self.y = y
-        self.id = None
+        """Initialization"""
         super().__init__(size, size, x, y, id)
 
     def __str__(self):
