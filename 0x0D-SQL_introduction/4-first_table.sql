9f447616419bc59779377d78eb5521f4772cb8ab
Michael Gebregiorgis <you@example.com>
2024-02-15 08:03:05 -0800

    create table

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,2 +1,2 @@
 -- creates a table called first_table
-CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` CHARVAR(256))
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` CHARVAR(256));
